b1f5a859741a4e791222a746aa4ab617f4c2520b
hurraira5 <hurraira10@icloud.com>
2026-03-03 13:00:55 +0500

    adaasaa

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const bcrypt = require("bcryptjs");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*", // Ye har jagah se request allow kar dega
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // ==========================================
