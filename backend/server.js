978191fdd94e6b9159dab1556fb83af7a662f055
hurraira5 <hurraira10@icloud.com>
2026-03-05 09:57:53 +0500

    cors fix68

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,12 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 app.use(express.json());
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+  next();
+});
 
 // ==========================================
 // DATABASE CONNECTION (NEON POSTGRESQL)
